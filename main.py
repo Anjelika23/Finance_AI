23a692f71246f89a381cfe977a7ff3aae2ea919a
Anjelika23 <anjelikadewanjee@gmail.com>
2026-03-26 19:46:24 +0530

    Update CORS origins in main.py
    Replaced localhost URL with a specific Netlify URL.

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from model import predict_loan
 app = Flask(__name__)
 CORS(app, origins=[
     "https://*.netlify.app",
-    "http://localhost:3000",
+    "https://glistening-cannoli-6bfc3b.netlify.app",
     "http://127.0.0.1:5500",
     "http://localhost:5500",
     os.environ.get("FRONTEND_URL", "")
