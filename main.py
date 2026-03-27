49f8e1b0c310c9c77877833e426aa338c858bcbb
Anjelika23 <anjelikadewanjee@gmail.com>
2026-03-27 19:44:19 +0530

    Update main.py

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ CORS(app, origins=[
     "http://localhost:3000",
     "http://127.0.0.1:5500",
     "http://localhost:5500",
+   "https://glistening-cannoli-6bfc3b.netlify.app/"
     os.environ.get("FRONTEND_URL", "")
 ])
 
